a923a4e5e04341b605e857f7b84bb8b71e11c179
Roberto-PM2 <39205282@uaz.edu.mx>
2023-10-14 16:59:48 -0600

    first commit

---
+++ VinculacionSS/Vinculacion/pruebas/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from pruebas import views
+
+
+urlpatterns = [
+    path('servicio/', views.servicio, name='servicio'),
+    path('hola/', views.mensaje),
+    path('inicio/', views.inicio, name='inicio'),  # Agregamos la URL para la vista 'inicio'
+    path('bolsatrabajo/', views.bolsatrabajo, name='bolsatrabajo'),  # Agregamos la URL para la vista 'bolsatrabajo'
+    path('seguridad/', views.seguridad, name='seguridad'),  # Agregamos la URL para la vista 'seguridad'
+    path('eb/', views.eb, name='eb'),  # Agregamos la URL para la vista 'eb'
+    path('area/', views.area, name='area'),  # Agregamos la URL para la vista 'area'
+    path('fortalecimiento/', views.fortalecimiento, name='fortalecimiento'),  # Agregamos la URL para la vista 'fortalecimiento'
+]
---
+++ VinculacionSS/Vinculacion/vinculacion_web/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+def mensaje(request):
+    return render(request, 'mensaje.html')
+
+def servicio(request):
+    return render(request, 'ServicioSocial.html')
+
+
+def inicio(request):
+    return render(request, 'MainPage.html')
+
+
+def bolsatrabajo(request):
+    return render(request, 'BolsaTrabajo.html')
+
+
+def seguridad(request):
+    return render(request, 'Seguridad.html')
+
+
+def eb(request):
+    return render(request, 'eb.html')
+
+def area(request):
+    return render(request, 'AreaAdministrativa.html')
+
+def fortalecimiento(request):
+    return render(request, 'fortalecimiento.html')
